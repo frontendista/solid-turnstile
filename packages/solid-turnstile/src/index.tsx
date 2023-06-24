a5b1763a4d8887b36529b2307bc7f4205c4d4c9f
Pavel Sušický <me@frontendista.cz>
2023-06-24 10:28:26 +0200

    feat(package): add component

--- packages/solid-turnstile/src/index.tsx
+++ packages/solid-turnstile/src/index.tsx
@@ -1,22 +1,61 @@
 import {
   createEffect,
   createSignal,
+  mergeProps,
+  on,
   onCleanup,
   onMount,
   Show,
+  splitProps,
   type VoidComponent,
 } from "solid-js";
-import type { TurnstileOptions } from "turnstile-types";
 
-declare global {
-  interface Window {
-    onTurnstileLoaded: () => void;
-  }
-}
-
-type TurnstileProps = TurnstileOptions;
+type TurnstileProps = {
+  siteKey: string;
+  /**
+   * @default "onLoadTurnstileCallback"
+   */
+  onLoadCallbackName?: string;
+  /**
+   * onSuccess is called when the challenge is successfully solved.
+   */
+  onSuccess?: (token: string) => void;
+  /**
+   * onError is called when an error occurs during the challenge or connecting to Turnstile.
+   */
+  onError?: () => void;
+  /**
+   * onExpire is called when token issued by successful challenge expires.
+   */
+  onExpire?: () => void;
+};
 
 const TURNSTILE_URL = "https://challenges.cloudflare.com/turnstile/v0/api.js";
+
+const [turnStileState, setState] = createSignal<
+  "loading" | "loaded" | "error" | "none"
+>("none");
+
+function injectScript(callback: string) {
+  const turnstile = document.getElementById("cf-turnstile");
+
+  if (turnstile) return;
+
+  setState("loading");
+
+  const script = document.createElement("script");
+  script.src = `${TURNSTILE_URL}?onload=${callback}&render=explicit`;
+  script.async = true;
+  script.defer = true;
+  script.id = "cf-turnstile";
+
+  // @ts-expect-error
+  window[callback] = () => setState("loaded");
+
+  script.onerror = () => setState("error");
+
+  document.body.appendChild(script);
+}
 
 /**
  * Explicetely renders Turnstile widget.
@@ -25,34 +64,47 @@ const TURNSTILE_URL = "https://challenges.cloudflare.com/turnstile/v0/api.js";
  */
 export const Turnstile: VoidComponent<TurnstileProps> = (props) => {
   let element: HTMLDivElement;
-  let [isTurnstileLoaded, setLoaded] = createSignal(false);
+  let [widgetId, setWidgetId] = createSignal<string | null>(null);
 
-  onMount(() => {
-    if ("turnstile" in window) {
-      setLoaded(true);
-    } else {
-      const script = document.createElement("script");
+  const [local, attributes] = splitProps(props, [
+    "siteKey",
+    "onLoadCallbackName",
+    "onSuccess",
+    "onError",
+    "onExpire",
+  ]);
 
-      script.src = `${TURNSTILE_URL}?render=explicit&onload=onTurnstileLoaded`;
-      script.async = true;
-      script.defer = true;
-
-      document.head.appendChild(script);
-
-      window.onTurnstileLoaded = () => {
-        setLoaded(true);
-      };
-    }
+  const cf = mergeProps(local, {
+    onLoadCallbackName: "onLoadTurnstileCallback",
   });
 
-  createEffect(() => {
-    if (isTurnstileLoaded()) {
-      window.turnstile.render(element, props);
-    }
-  });
+  onMount(() => injectScript(cf.onLoadCallbackName));
+
+  createEffect(
+    on(turnStileState, () => {
+      if (!window.turnstile) return;
+
+      const id = window.turnstile.render(element, {
+        sitekey: cf.siteKey,
+        callback: cf.onSuccess,
+        "error-callback": cf.onError,
+        "expired-callback": cf.onExpire,
+      });
+
+      if (!id) {
+        cf.onError?.();
+      }
+
+      setWidgetId(id);
+
+      onCleanup(() => {
+        window.turnstile.remove(id);
+      });
+    })
+  );
 
   return (
-    <Show when={isTurnstileLoaded}>
+    <Show when={turnStileState()}>
       <div ref={element!} />
     </Show>
   );
--- websites/playground/src/main.tsx
+++ websites/playground/src/main.tsx
@@ -1,7 +1,54 @@
 import { render } from "solid-js/web";
 import { Turnstile } from "../../../packages/solid-turnstile/src/index";
+import { Show, createSignal } from "solid-js";
 
-render(
-  () => <Turnstile sitekey="1x00000000000000000000AA" />,
-  document.getElementById("app")!
-);
+const App = () => {
+  const [visible, setVisibility] = createSignal(true);
+
+  return (
+    <main>
+      <Show when={visible()}>
+        <div>
+          <h1>Visible, passes.</h1>
+          <Turnstile
+            siteKey="1x00000000000000000000AA"
+            onSuccess={console.log}
+            onError={() => console.warn("Error: Nějak se to pokazilo")}
+            onExpire={() => console.warn("Expirovalo to :(")}
+          />
+        </div>
+        <div>
+          <h1>Visible, blocks.</h1>
+          <Turnstile
+            siteKey="2x00000000000000000000AB"
+            onSuccess={console.log}
+            onError={() => console.warn("Error: Nějak se to pokazilo")}
+            onExpire={() => console.warn("Expirovalo to :(")}
+          />
+        </div>
+        <div>
+          <h1>Invisible, passes.</h1>
+          <Turnstile
+            siteKey="1x00000000000000000000BB"
+            onSuccess={console.log}
+            onError={() => console.warn("Error: Nějak se to pokazilo")}
+            onExpire={() => console.warn("Expirovalo to :(")}
+          />
+        </div>
+        <div>
+          <h1>Invisible, blocks.</h1>
+          <Turnstile
+            siteKey="2x00000000000000000000BB"
+            onSuccess={console.log}
+            onError={() => console.warn("Error: Nějak se to pokazilo")}
+            onExpire={() => console.warn("Expirovalo to :(")}
+          />
+        </div>
+      </Show>
+
+      <button onClick={() => setVisibility((value) => !value)}>Toggle</button>
+    </main>
+  );
+};
+
+render(() => <App />, document.getElementById("app")!);
